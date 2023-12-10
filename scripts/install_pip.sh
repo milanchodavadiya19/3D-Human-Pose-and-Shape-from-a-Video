7f581d3986d5cdf9d51e4638e0097f41fa8b309b
Milan Chodavadiya <120493459+milanchodavadiya19@users.noreply.github.com>
2023-12-10 23:14:30 +0100

    Update install_pip.sh

--- scripts/install_pip.sh
+++ scripts/install_pip.sh
@@ -7,6 +7,7 @@ python3.9 -m venv tcmr-env
 
 source $PWD/tcmr-env/bin/activate
 
+
 $PWD/tcmr-env/bin/pip install torch==1.12.1+cu113 torchvision==0.13.1+cu113 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu113
 $PWD/tcmr-env/bin/pip install git+https://github.com/giacaglia/pytube.git --upgrade
 $PWD/tcmr-env/bin/pip install -r requirements.txt
